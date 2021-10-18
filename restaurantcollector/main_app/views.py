43bd7cb31860c62dd323e735426e457fdc54530b
Britt Ramsubick <bramsubic@gmail.com>
2021-10-18 19:39:21 -0400

    part 1 complete

--- restaurantcollector/main_app/views.py
+++ restaurantcollector/main_app/views.py
@@ -3,6 +3,19 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+class Restaurant:  
+  def __init__(self, name, neighbourhood, cost, cuisine, suggestion):
+    self.name = name
+    self.neighbourhood = neighbourhood
+    self.cost = cost
+    self.cuisine = cuisine
+    self.suggestion = suggestion
+
+restaurants = [
+  Restaurant('Konjiki Ramen', 'North York', '$$', 'Japanese', 'Spicy Red Tonkotsu Ramen'),
+  Restaurant('Foxley', 'Dundas St W', '$$', 'Asian & Pan-Latin', 'Arctic Char Ceviche with Green Apple and Ginger'),
+  Restaurant('Raku', 'Queen St W', '$$', 'Japanese', 'Niku')
+]
 # Define the home view
 #  defines a view func that responds to browser's HTTP request 
 def home(request):
@@ -10,3 +23,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def restaurants_index(request):
+  return render(request, 'restaurants/index.html', { 'restaurants': restaurants })
