d015b5c2d50f085a4b5e9b4394db95b72b8483da
Britt Ramsubick <bramsubic@gmail.com>
2021-10-18 16:14:41 -0400

    defin views

--- restaurantcollector/main_app/urls.py
+++ restaurantcollector/main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path #will be used to deine each route 
 from . import views
 
-urlpatterns = [ #will hold each route we define for main_app
+#will hold each route we define for main_app
+urlpatterns = [ 
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- restaurantcollector/main_app/views.py
+++ restaurantcollector/main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+#  defines a view func that responds to browser's HTTP request 
+def home(request):
+  return HttpResponse('<h1>Welcome to the Resto Collective</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Resto Collective</h1>')
