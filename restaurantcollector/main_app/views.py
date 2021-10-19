bfcbd0058a9c8ac3184c25c448e54ae1703f4e43
Britt Ramsubick <bramsubic@gmail.com>
2021-10-18 21:52:20 -0400

    part 2 done

--- restaurantcollector/main_app/views.py
+++ restaurantcollector/main_app/views.py
@@ -12,3 +12,7 @@ def about(request):
 def restaurants_index(request):
   restaurants = Restaurant.objects.all()
   return render(request, 'restaurants/index.html', { 'restaurants': restaurants })
+
+def restaurants_detail(request, restaurant_id):
+  restaurant = Restaurant.objects.get(id=restaurant_id)
+  return render(request, 'restaurants/detail.html', { 'restaurant': restaurant })
