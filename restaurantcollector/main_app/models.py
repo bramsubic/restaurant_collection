f4cde9e0b8d8762690ff545ecaedcb12333a6da5
Britt Ramsubick <bramsubic@gmail.com>
2021-10-18 21:25:20 -0400

    create the cmodel

--- restaurantcollector/main_app/models.py
+++ restaurantcollector/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    neighbourhood = models.CharField(max_length=100)
+    cost = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    suggestion = models.TextField(max_length=250)
+    
+    def __str__(self):
+        return self.name
--- restaurantcollector/main_app/views.py
+++ restaurantcollector/main_app/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render
+from .models import Restaurant
 
-# Add the following import
-from django.http import HttpResponse
-
-class Restaurant:  
-  def __init__(self, name, neighbourhood, cost, cuisine, suggestion):
-    self.name = name
-    self.neighbourhood = neighbourhood
-    self.cost = cost
-    self.cuisine = cuisine
-    self.suggestion = suggestion
-
-restaurants = [
-  Restaurant('Konjiki Ramen', 'North York', '$$', 'Japanese', 'Spicy Red Tonkotsu Ramen'),
-  Restaurant('Foxley', 'Dundas St W', '$$', 'Asian & Pan-Latin', 'Arctic Char Ceviche with Green Apple and Ginger'),
-  Restaurant('Raku', 'Queen St W', '$$', 'Japanese', 'Niku')
-]
 # Define the home view
 #  defines a view func that responds to browser's HTTP request 
 def home(request):
-  return HttpResponse('<h1>Welcome to the Resto Collective</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def restaurants_index(request):
+  restaurants = Restaurant.objects.all()
   return render(request, 'restaurants/index.html', { 'restaurants': restaurants })
