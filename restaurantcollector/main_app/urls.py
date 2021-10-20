8e0687c2434d7bc062ae7963812c071d3e418975
Britt Ramsubick <bramsubic@gmail.com>
2021-10-20 18:21:34 -0400

    add create funct

--- restaurantcollector/main_app/urls.py
+++ restaurantcollector/main_app/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('restaurants/', views.restaurants_index, name='index'),
     path('restaurants/<int:restaurant_id>/', views.restaurants_detail, name='detail'),
+    path('restaurants/new', views.restaurants_new),
+    path('restaurants/submit/', views.restaurants_create),  
+
+  # these urls are connected to view functions
+    path('restaurants/<int:restaurant_id>/delete/', views.restaurants_delete, name='restaurants_delete'),
+    path('restaurants/<int:restaurant_id>/edit/', views.restaurants_edit, name='restaurants_edit'), #show the form
+    path('restaurants/<int:restaurant_id>/update/', views.restaurants_update, name='restaurants_update') #handle form data
 ]
--- restaurantcollector/main_app/views.py
+++ restaurantcollector/main_app/views.py
@@ -1,7 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import Restaurant
 
-# Define the home view
+# View functions
+def restaurants_delete(request,restaurant_id):
+  # get ahold of the resto
+  r = Restaurant.objects.get(id=restaurant_id)  
+  r.delete() # <--- django ORM command to delete from DB
+  return redirect('/restaurants')
+
+def restaurants_new(request):
+  return render(request, 'restaurants/new.html')
+
+# show form
+def restaurants_edit(request, restaurant_id):
+  r = Restaurant.objects.get(id= restaurant_id)
+  print("we are updating resto", r)
+  return render(request, 'restaurants/edit.html', {
+    'restaurant':r
+  })
+
+# handle form data
+def restaurants_update(request, restaurant_id):
+  r = Restaurant.objects.get(id = restaurant_id)
+  r.name = request.POST['name']
+  r.neighbourhood = request.POST['neighbourhood']
+  r.cost = request.POST['cost']
+  r.cuisine = request.POST['cuisine']
+  r.suggestion = request.POST['suggestion']
+  r.save() # <---- save to db
+  return redirect(f'/restaurants/{r.id}')
+
+def restaurants_create(request):
+  # request.POST is django's req.body 
+  # it contains incoming form data
+  print("ctrl hit")
+  print(request.POST)
+  r = Restaurant.objects.create(
+    name= request.POST['name'],
+    neighbourhood = request.POST['neighbourhood'],
+    cost = request.POST['cost'],
+    cuisine = request.POST['cuisine'],
+    suggestion = request.POST['suggestion']
+  )
+  # create a RESTO object out of the 
+  #    incoming data
+  # redirect to a different URL
+  return redirect(f'/restaurants/{r.id}')
+
 #  defines a view func that responds to browser's HTTP request 
 def home(request):
   return render(request, 'home.html')
